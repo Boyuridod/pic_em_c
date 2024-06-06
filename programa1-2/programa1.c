14809e4e34dbb8130220d102526f645592e3c6a6
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-06-06 17:39:28 -0300

    Novos trabalhos adicionados

--- programa1-2/programa1.c
+++ programa1-2/programa1.c
@@ -1,44 +1,53 @@
 #include <programa1.h>
 
-int8 centena = 0, dezena = 0, unidade = 0, decimal = 0, ligado = 0, tempo = 0;
+int1 ligado = 0;
+int8 qual_display = -1, tempo = 0;
+int8 centena = 0, dezena = 0, unidade = 0, decimal = 0;
 int8 conversao[10] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
-int16 contador = 9900;
-
-void extrai_numero(int16 num){
-
-   centena = (int)(num / 1000);
-   dezena = (int)((num - (centena * 1000)) / 100);
-   unidade = (int)((num - (centena * 1000) - (dezena * 100)) / 10);
-   decimal = (int)(num - (centena * 1000) - (dezena * 100) - (unidade * 10));
-
-}
-
-//4 FOR INCREMENTANDO AS VARIAVEIS
 
 #INT_TIMER0
 void  TIMER0_isr(VOID) {
 
    tempo++;
 
-   if(tempo == 100){
+   if(tempo == 100 && ligado == 1){
 
-      contador++;
+      decimal++;
 
-      if(contador > 9999){
-      
-         contador = 0;
-      
+      if(decimal == 10){
+
+         decimal = 0;
+         unidade++;
+
       }
 
+      if(unidade == 10){
+
+         unidade = 0;
+         dezena++;
+
+      }
+
+      if(dezena == 10){
+
+         dezena = 0;
+         centena++;
+
+      }
+
+      if(centena == 10){
+
+         decimal = unidade = dezena = centena = 0;
+
+      }
+      
       tempo = 0;
 
    }
 
-   extrai_numero(contador);
+   qual_display++;
 
-   ligado++;
-
-   if(ligado == 0){
+   if(qual_display == 0){
 
       output_low(DISPLAY4);
       output_high(DISPLAY1);      
@@ -46,7 +55,7 @@ void  TIMER0_isr(VOID) {
 
    }
 
-   else if(ligado == 1){
+   else if(qual_display == 1){
 
       output_low(DISPLAY1);
       output_high(DISPLAY2);   
@@ -55,7 +64,7 @@ void  TIMER0_isr(VOID) {
 
    }
 
-   else if(ligado == 2){
+   else if(qual_display == 2){
   
       output_low(DISPLAY2);
       output_high(DISPLAY3);      
@@ -63,13 +72,13 @@ void  TIMER0_isr(VOID) {
 
    }
 
-   else if(ligado == 3){
+   else if(qual_display == 3){
  
       output_low(DISPLAY3);
       output_high(DISPLAY4);      
       output_d(conversao[centena]);
 
-      ligado = -1;
+      qual_display = -1;
 
    }
 
@@ -83,17 +92,24 @@ void main(){
        
       if(!input(BTN_INICIAR)){
       
-         
+         ligado = 1;
          
       }
       
       else if(!input(BTN_PARAR)){
       
+         ligado = tempo = 0;
       
       }
       
       else if(!input(BTN_ZERAR)){
       
+         if(ligado == 0){
+      
+            decimal = unidade = dezena = centena = 0;
+            tempo = 0;
+         
+         }
       
       }
    }
